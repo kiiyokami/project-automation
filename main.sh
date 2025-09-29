9113a04bb7ba3089677ae90f664fdb066a04c765
Kiiyo <85468568+kiiyokami@users.noreply.github.com>
2025-09-29 16:02:59 +0800

    reworked to use github based boilerplates

--- main.sh
+++ main.sh
@@ -2,180 +2,104 @@
 
 # source sourcing out bashrc and compilers
 source ~/.bashrc
-source ~/.bash_profile
 source ~/.nvm/nvm.sh
 
-echo Project Creation Automation
-echo =========================
+echo Project Creation Automation Script
+echo ===========================
 
 #Move out of the current directory
 cd ..
 
-# Loop until the user enters a valid project name
-while true; do
-  echo Enter the project name:
-  echo =========================
-  read PROJECT_NAME
 
-  # Check if the project name is valid
-  if [ -d "$PROJECT_NAME" ]; then
-    echo "Project name already exists. Please choose a different name."
-    continue
-  # Check if project name has a space
-  elif [[ "$PROJECT_NAME" == *" "* ]]; then
-    echo "Project name cannot contain spaces. Please choose a different name."
-    continue
-  else
-    break
-  fi
-done
-
-# Create a new directory
-mkdir $PROJECT_NAME
-cd $PROJECT_NAME
+#Create the project directory
+read -p "Enter the project name: " project_name
 
 
-# Initialize the git repository
-git init
+# Check if project name has spaces, then replace with underscore
+if [[ $project_name == *" "* ]]; then
+    project_name=${project_name// /_}
+fi
+
+if [ -d "$project_name" ]; then
+    rm -rf $project_name
+fi
+
+mkdir $project_name
+cd $project_name
+
+clear
+echo "Choose a template"
+echo "================="
+
+echo "(1) Frontend Template (2) Backend Template"
+read -p "Enter your choice: " template
 clear
 
-# Ask for the project type
-echo What type of project is this?
-echo [1] Frontend - Static File Hosting Only
-echo [2] Backend - For TypeScript, Python, or Golang
-echo =========================
-read PROJECT_TYPE
+case $template in
+    1)
+        echo "Frontend Template Selected"
+        echo "==========================="
+        curl -qL https://www.npmjs.com/install.sh | sh
+        npm init vite@latest .
+        npm install
+        git init
+        echo "Frontend project created. Run 'npm run dev' to start development server."
+        ;;
+    2)
+        echo "Backend Template Selected"
+        echo "==========================="
+        echo "Choose a language"
+        echo "(1) Node.js (2) Python (3) Go"
+        read -p "Enter your choice: " language
+        clear
+        case $language in
+            1)
+                echo "Node.js Template Selected"
+                echo "Do you want to use npm, deno, or bun?"
+                echo "(1) npm (2) deno (3) bun"
+                read -p "Enter your choice: " npm_or_deno_or_bun
+                clear
+                case $npm_or_deno_or_bun in
+                    1)
+                        curl -qL https://www.npmjs.com/install.sh | sh
+                        source ~/.bashrc
+                        echo "npm installed, using npm init"
+                        npm init -y
+                        touch index.js
+                        git init
+                        ;;
+                    2)
+                        curl -fsSL https://deno.land/install.sh | sh
+                        git clone git@github.com:lampewebdev/deno-boilerplate.git .
+                        git init
+                        ;;
+                    3)
+                        curl -fsSL https://bun.sh/install | bash
+                        git clone git@github.com:RajaRakoto/bun-boilerplate.git .
+                        git init
+                        ;;
+                    *)
+                        echo "Invalid choice"
+                        exit 1
+                        ;;
+                esac
+                ;;
+            2)  
+                echo "Python Template Selected"
+                git clone git@github.com:Dugnist/python-pipenv-starter.git .
+                git init
+                ;;
+            3)
+                echo "Go Template Selected"
+                git clone git@github.com:kazmerdome/best-ever-golang-starter.git .
+                git init
+                ;;
+        esac
+        ;;
+    *)
+        echo "Invalid choice"
+        exit 1
+        ;;
+esac
 
-if [ $PROJECT_TYPE -eq 1 ]; then
-  echo Creation in progress...
-  #Create project files
-  touch index.html
-  touch style.css
-  touch script.js
-  mkdir assets
-
-  #Add boilerplate code
-  echo "<!DOCTYPE html>
-    <html>
-        <head>
-            <title>Hello, World!</title>
-            <link rel=\"stylesheet\" href=\"style.css\">
-        </head>
-        <body>
-            <h1>Hello, World!</h1>
-            <script src=\"script.js\"></script>
-        </body>
-    </html>" >index.html
-
-  echo "body {
-        background-color: black;
-        color: white;
-    }" >style.css
-  echo "console.log('Hello, World!');" >script.js
-
-  echo "Successfully created the project files."
-
-elif [ $PROJECT_TYPE -eq 2 ]; then
-  echo Backend Project
-  echo ===============
-  # Ask for the programming language
-  echo What programming language is this?
-  echo [1] TypeScript
-  echo [2] Python
-  echo [3] Golang
-  echo =========================
-  read PROJECT_LANGUAGE
-
-  if [ $PROJECT_LANGUAGE -eq 1 ]; then
-    echo Creation in progress...
-    #Create project files
-    mkdir src
-    touch src/index.ts
-    # Add boilerplate typescript + express code
-    echo "import express from 'express';
-
-  const app = express();
-  app.get('/', (req: express.Request, res: express.Response) => {
-    res.send('Hello, World!');
-  });
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');    
-  });" >src/index.ts
-    
-    # Add a tsconfig.json file
-    touch tsconfig.json
-    # Add a compilerOptions section to the tsconfig.json file
-    echo "{
-  \"compilerOptions\": {
-  \"outDir\": \"./dist\",
-  \"rootDir\": \"./src\",
-  \"target\": \"es2022\",
-  \"module\": \"commonjs\",
-  \"strict\": true,
-  \"esModuleInterop\": true,
-  \"skipLibCheck\": true,
-  \"forceConsistentCasingInFileNames\": true
-  }
-}" >tsconfig.json
-    #Run npm init
-    npm init -y
-    #Install express and typescript dependency
-    npm install express
-    npm i --save-dev @types/express
-    #Install typescript
-    npm install typescript
-    #Install ts-node
-    npm install ts-node
-
-    #Compile the TypeScript files
-    npx tsc
-
-    # Gitignore compiled files
-    echo "node_modules/
-index.js
-package-lock.json
-dist" >.gitignore
-
-    echo "Successfully created the project files."
-    echo "You can now run the server using the command 'node index.js'."
-    echo "The server will be running on port 3000."
-    echo "Press any key to exit."
-    read
-    exit
-  elif [ $PROJECT_LANGUAGE -eq 2 ]; then
-    echo Python Project
-    echo =============
-    #Create pipenv
-    pipenv install
-    #Configure pipenv
-    pipenv shell
-    #Create project files
-    touch main.py
-    # Add boilerplate code
-    echo "from flask import Flask
-    app = Flask(__name__)
-    @app.route('/')
-    def hello_world():    
-    return 'Hello, World!'" >main.py
-    touch requirements.txt
-    touch Pipfile
-    touch Pipfile.lock
-  elif [ $PROJECT_LANGUAGE -eq 3 ]; then
-    echo Golang Project
-    echo =============
-    #Create project files
-    touch main.go
-    touch go.mod
-    touch go.sum
-  fi
-fi
-
-echo Do you want to open the project directory in VS Code?
-echo [1] Yes
-echo [2] No
-read OPEN_PROJECT
-
-if [ $OPEN_PROJECT -eq 1 ]; then
-  code .
-fi
-
+echo "Project '$project_name' created successfully!"
